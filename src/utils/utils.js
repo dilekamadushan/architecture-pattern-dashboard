f3a9d69d8c4053728b3ca28836f242955716ddc9
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2022-03-08 16:05:08 +0530

    refactor numbers in the logic

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,31 +27,28 @@ const coordinationRecommendations = (userInput) => {
 
     return coordination;
 }
-/* tableData={[
-                  ["1", "Event Driven State Management", "High scalability, contracts can be abstracted into events"],
-                  ["2", "Event Sourcing", "Single source of truth(Recommended for banking systems)"],
-                ]}*/
+
 const stateManagementRecommendations = (userInput) => {
     const recommendations = [];
 
     switch (userInput.domain) {
-        case "0" || "2" || "3":
+        case 0 || 2 || 3:
             recommendations.push([(recommendations.length+1).toString(), "Event Driven State Management", "High scalability, contracts can be abstracted into events"]);
             break;
         default:
             console.log("Domain is not known into state management");
     }
     switch (userInput.teamExperience) {
-        case "0":
+        case 0:
             console.log("team experience is low");
             recommendations.push([(recommendations.length+1).toString(), "Message oriented state patterns", "This should be used by an organization beginning the microservices journey, but it may not suit your needs as your approach becomes more mature"]);
             break;
         default:
-            console.log("team experience is high");
+            console.log("team experience is high", userInput.teamExperience);
             recommendations.push([(recommendations.length+1).toString(), "Event Sourcing", "Single source of truth(Recommended for banking systems)"]);
     }
     switch (userInput.boundaryContexts) {
-        case "1":
+        case 1:
             console.log("boundaryContexts");
             recommendations.push([(recommendations.length+1).toString(), "SAGA Event pattern is preferred", "Since transactions spanning among multiple services could be addressed by SAGA pattern"]);
             break;
@@ -70,7 +67,7 @@ const deploymentRecommendations = (userInput) => {
     const recommendations = [];
 
     switch (userInput.cost) {
-        case "0":
+        case 0:
             recommendations.push(["1", "Serverless deployment", "Low cost, High Scalability, Low warm up time"]);
             console.log("low cost");
             break;
@@ -87,7 +84,7 @@ const technologyRecommendations = (userInput) => {
     const recommendations = [];
 
     switch (userInput.cost) {
-        case "0":
+        case 0:
             recommendations.push([(recommendations.length+1).toString(), "AWS Lambda, Serverless", "Low cost, High Scalability, Low warm up time"]);
             console.log("low cost");
             break;
@@ -96,7 +93,7 @@ const technologyRecommendations = (userInput) => {
     }
 
     switch (userInput.dataScalability) {
-        case "1":
+        case 1:
             recommendations.push([(recommendations.length+1).toString(), "No SQL databases(MongoDB, Cassandra)", "High vertical scalability"]);
             break;
         default:
@@ -104,7 +101,7 @@ const technologyRecommendations = (userInput) => {
             console.log("not NoSQL");
     }
     switch (userInput.dataAcid) {
-        case "1":
+        case 1:
             recommendations.push([(recommendations.length+1).toString(), "SQL databases(MySQl, Postgress)", "Ensures ACID properties"]);
             break;
         default:
@@ -123,14 +120,14 @@ const dataStorageRecommendations = (userInput) => {
     const recommendations = [];
 
     switch (userInput.dataScalability) {
-        case "1":
+        case 1:
             recommendations.push([(recommendations.length+1).toString(), "Database per service pattern", "The success of this pattern hinges on effectively defining the bounded contexts in your application"]);
             break;
         default:
             recommendations.push([(recommendations.length+1).toString(), "Database cluster pattern", "All the microservices can use the same database, each microserive sees it as its own datastore "]);
     }
     switch (userInput.dataAcid) {
-        case "1":
+        case 2:
             recommendations.push([(recommendations.length+1).toString(), "SQL databases(MySQl, Postgress)", "Ensures ACID properties"]);
             break;
         default:
